c7f57d21e8f8d256e7adcf43083ac82a6e06db51
bokem29 <byandnadif28@gmail.com>
2025-01-19 14:18:08 +0700

    first commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
+import { useFonts } from 'expo-font'; 
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'jakarta-regular': require('./assets/fonts/PlusJakartaSans-Regular.ttf'),
+    'jakarta-medium': require('./assets/fonts/PlusJakartaSans-Medium.ttf'),
+    'jakarta-semibold': require('./assets/fonts/PlusJakartaSans-SemiBold.ttf'),
+    'jakarta-bold': require('./assets/fonts/PlusJakartaSans-Bold.ttf'),
+    'jakarta-extrabold': require('./assets/fonts/PlusJakartaSans-ExtraBold.ttf'),
+    'jakarta-light': require('./assets/fonts/PlusJakartaSans-Light.ttf'),
+    'jakarta-italic': require('./assets/fonts/PlusJakartaSans-Italic.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return <ActivityIndicator size="large" color="#0000ff" />; 
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.text}>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +31,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text:{
+    fontFamily: 'jakarta-regular'
+  }
 });
